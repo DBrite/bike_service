34d882744b6142c0dbb4bd023735b47783316a8b
DBrite <paragonbright1999@gmail.com>
2021-07-06 20:40:24 -0700

    models and template

--- bike_service/settings.py
+++ bike_service/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bike_app.apps.BikeAppConfig'
+
 ]
 
 MIDDLEWARE = [
--- bike_service/urls.py
+++ bike_service/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('bike_app/', include('bike_app.urls')),
+    path('', RedirectView.as_view(url='bike_app/', permanent=True)),
+
+] +     static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
